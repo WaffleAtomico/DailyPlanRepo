49c88afe7667d541f9fb9c44c5298dcc371b4c5b
DarkAbsolutE <dark_necros_absolute@hotmail.com>
2024-07-31 19:43:09 -0600

    Se agregan notificaicones por día para alarmas

--- DailyPlan/dailyplan/src/views/UI/useNotificationChecker.js
+++ DailyPlan/dailyplan/src/views/UI/useNotificationChecker.js
@@ -1,6 +1,8 @@
 // useNotificationChecker.js
 import { useState, useEffect, useRef } from 'react';
 import { getUserNotifications } from '../../utils/validations/notification';
+import { getAlarmsForUser } from '../../utils/validations/alarm';
+import { getDaySelectedById } from '../../utils/validations/dayselected';
 import { myPojo } from '../../utils/ShowNotifInfo';
 
 const useNotificationChecker = (id) => {
@@ -10,6 +12,7 @@ const useNotificationChecker = (id) => {
   useEffect(() => {
     const fetchNotifications = () => {
       userNotifications(id);
+      alarmNotifications(id);
     };
     const intervalId = setInterval(fetchNotifications, 10000); // 120000ms = 2 minutos
     return () => clearInterval(intervalId);
@@ -40,6 +43,56 @@ const userNotifications = (id) => {
             myPojo.setNotif(`Cancelación ${newNotif.name}`, <>{newNotif.name}</>);
             displayedNotifs.current.add(newNotif.id); // Marcar la notificación como mostrada
           }
+        });
+      }
+      setInitialLoad(false);
+    }
+  }).catch(err => { console.log(err) });
+};
+
+const alarmNotifications = (id) => {
+  getAlarmsForUser(id).then(res => {
+    console.log("Alarm Check");
+    if (res.status) {
+      if (!initialLoad) {
+        res.data.forEach(alarma => {
+          getDaySelectedById(alarma.daysel_id).then(res_days => {
+            const date = new Date();
+            const dias=["dom", "lun", "mar", "mie", "jue", "vie", "sab"];
+
+            const day = dias[date.getUTCDate()];
+            const hour = String(date.getHours()).padStart(2, '0');
+            const min = String(date.getMinutes()).padStart(2, '0');
+
+            if (res_days.daysel_mon == 1 && day == "lun" && hour == alarma.alarm_hour && min == alarma.alarm_min) {
+              myPojo.setNotif("Alarma", <>{alarma.alarm_name}</>);
+              displayedNotifs.current.add(alarma.alarm_id); // Marcar la notificación como mostrada
+            }
+            if (res_days.daysel_tues == 1 && day == "mar" && hour == alarma.alarm_hour && min == alarma.alarm_min) {
+              myPojo.setNotif("Alarma", <>{alarma.alarm_name}</>);
+              displayedNotifs.current.add(alarma.alarm_id); // Marcar la notificación como mostrada
+            }
+            if (res_days.daysel_wed == 1 && day == "mie" && hour == alarma.alarm_hour && min == alarma.alarm_min) {
+              myPojo.setNotif("Alarma", <>{alarma.alarm_name}</>);
+              displayedNotifs.current.add(alarma.alarm_id); // Marcar la notificación como mostrada
+            }
+            if (res_days.daysel_thur == 1 && day == "jue" && hour == alarma.alarm_hour && min == alarma.alarm_min) {
+              myPojo.setNotif("Alarma", <>{alarma.alarm_name}</>);
+              displayedNotifs.current.add(alarma.alarm_id); // Marcar la notificación como mostrada
+            }
+            if (res_days.daysel_fri == 1 && day == "vie" && hour == alarma.alarm_hour && min == alarma.alarm_min) {
+              myPojo.setNotif("Alarma", <>{alarma.alarm_name}</>);
+              displayedNotifs.current.add(alarma.alarm_id); // Marcar la notificación como mostrada
+            }
+            if (res_days.daysel_sat == 1 && day == "sab" && hour == alarma.alarm_hour && min == alarma.alarm_min) {
+              myPojo.setNotif("Alarma", <>{alarma.alarm_name}</>);
+              displayedNotifs.current.add(alarma.alarm_id); // Marcar la notificación como mostrada
+            }
+            if (res_days.daysel_sun == 1 && day == "dom" && hour == alarma.alarm_hour && min == alarma.alarm_min) {
+              myPojo.setNotif("Alarma", <>{alarma.alarm_name}</>);
+              displayedNotifs.current.add(alarma.alarm_id); // Marcar la notificación como mostrada
+            }
+          }).catch(err => { console.log(err) });
         });
       }
       setInitialLoad(false);
